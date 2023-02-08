92a83638e860ec99d27b49aa571c943903c60d08
Moritz Hardt <m@mrtz.org>
2023-02-08 17:18:26 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="folktables",
     version="0.0.12",
     author="John Miller, Frances Ding, Ludwig Schmidt, Moritz Hardt",
-    author_email="miller_john@berkeley.edu",
+    author_email="hardt@is.mpg.de",
     description="New machine learning benchmarks from tabular datasets.",
     packages=find_packages(),
     install_requires=[
